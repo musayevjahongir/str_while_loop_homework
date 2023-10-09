a7c9487cd61dd3967349329cb19d597725ad4c4e
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-09 19:27:23 +0500

    raqamlar sonini aniqlash

--- While01.py
+++ While01.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    i=0
+    while i<len(s):
+        if s[i].isdigit():
+            a+=1
+        i+=1
+    return a
+print(main("python 2022"))
