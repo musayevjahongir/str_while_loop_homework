261dd873ae28680feb3b3b733d25eb87526eeb14
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-09 20:01:29 +0500

    bosh harflarni aniqlash

--- While04.py
+++ While04.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    i=0
+    while i<len(s):
+        if s[i].isupper():
+            a+=1
+        i+=1
+    return a
+print(main("CodeschoolUz"))
