05475b988b905b7dec4443a9c67874350485a38c
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-09 20:19:25 +0500

    juft sonlar

--- While07.py
+++ While07.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    i=0
+    while i<len (s):
+        if int(s[i])%2==0:
+            a+=1
+        i+=1
+    return a
+print(main("56786543250"))
