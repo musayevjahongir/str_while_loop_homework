885667e7e61d94dfff90c440a8aaa3e501b85c8b
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-09 19:58:16 +0500

    tinish belgilari

--- While03.py
+++ While03.py
@@ -6,4 +6,17 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    i=0
+    while i<len(s):
+        if s[i].isalpha():
+            a+=1
+        i+=1
+    b=0
+    j=0
+    while j<len(s):
+        if s[j].isdigit():
+            b+=1
+        j+=1
+    return len(s)-a-b
+print(main("#hashtag@$"))
