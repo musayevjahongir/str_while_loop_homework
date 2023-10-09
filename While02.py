a2fcebdeb4336c607a3245ac49e79d3a9cd7a3b4
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-09 19:32:06 +0500

    harflar sonini aniqlash

--- While02.py
+++ While02.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    i=0
+    while i<len(s):
+        if s[i].isalpha():
+            a+=1
+        i+=1
+    return a
+print(main("Python 2022"))
